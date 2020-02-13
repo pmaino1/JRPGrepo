6788890703617d9e6824196bf3908201c618cc7f
Paul <pcmaino@gmail.com>
2020-02-13 17:10:07 -0500

    Reorganized some methods, added more placeholders in Units, and started work on the BattleAction interface that will be used for all in battle actions.

---
+++ JRPGTest/BattleAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JRPGTest
+{
+    interface BattleAction
+    {
+        int ApCost { get; set; }
+
+        void executeAction(); 
+    }
+}
